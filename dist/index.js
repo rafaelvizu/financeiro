710ae414c82a173830946e480828b3564dcb5410
Rafael Vizu <rafaelvizu2@gmail.com>
2022-12-21 16:05:02 -0300

    init

---
+++ dist/index.js
@@ -0,0 +1,8 @@
+"use strict";
+console.log('Hello World');
+let a = 'Hello World';
+let person = {
+    name: 'John',
+    age: 30
+};
+console.log(person);
---
+++ src/index.ts
@@ -0,0 +1,16 @@
+console.log('Hello World');
+
+let a:string = 'Hello World';
+
+interface Person {
+     name: string;
+     age: number;
+}
+
+let person: Person = {
+     name: 'John',
+     age: 30
+}
+
+
+console.log(person)
