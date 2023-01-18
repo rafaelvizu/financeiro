e6ee69d57283b38150606a6b4225d89c013bd9fe
Rafael Vizu <rafaelvizu2@gmail.com>
2023-01-18 14:51:54 -0300

    update

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -13,6 +13,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 app.use(compression());
+app.use(express.static('public')); 
 
 
 app.use('/category', categoryRoute);
