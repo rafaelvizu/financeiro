7b03ebd145b369a133cfd8414a7a13c1ae76623c
Rafael Vizú <rafaelvizu2@gmail.com>
2023-01-23 18:53:36 -0300

    update

--- frontend/src/services/api.jsx
+++ frontend/src/services/api.jsx
@@ -1,8 +1,9 @@
 import axios from 'axios';
 
+console.log()
 
 export default axios.create({
-     baseURL: 'http://localhost:3000/',
+     baseURL: import.meta.env.VITE_API_LINK, 
      timeout: 5000,
      headers: {
           'x-acess-token': import.meta.env.VITE_PASSWORD
